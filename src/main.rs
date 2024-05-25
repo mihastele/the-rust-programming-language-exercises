217e3a5ae0fa772af9870bed28af05994d290254
Miha <stele1000@gmail.com>
2024-05-25 22:13:04 +0200

    guessing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut guesses_left = 10;
+    loop {
+        println!("Guess the number!");
+    
+        println!("Please input your guess.");
+    
+        let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+
+        guesses_left -= 1;
+        if guesses_left <= 0 {
+            println!("You lose!");
+            println!("The number was {secret_number}");
+            break;
+        }
+
+        print!("you have {guesses_left} guesses left\n")
+    }
 }

cc2a8823dd79ac0b5d117bfcf69203752a31ff53
Miha <stele1000@gmail.com>
2024-08-18 11:25:53 +0200

    WIP: Chapter 16

--- src/chapter16_fearless_concurrency.rs
+++ src/chapter16_fearless_concurrency.rs
@@ -2,7 +2,21 @@ use std::thread;
 use std::time::Duration;
 
 pub fn using_threads_to_run_code_simultaneously() {
-    thread::spawn(|| {
+
+    // if main process ends, the threadas are automatically killed
+    // thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hi number {i} from the spawned thread!");
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+    //
+    // for i in 1..5 {
+    //     println!("hi number {i} from the main thread!");
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from the spawned thread!");
             thread::sleep(Duration::from_millis(1));
@@ -13,4 +27,7 @@ pub fn using_threads_to_run_code_simultaneously() {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
+
+    // wait for the spawned thread to finish (blocks the main thread)
+    handle.join().unwrap();
 }

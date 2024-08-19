9933fd37d70c25116c04f771653058abd9d8fff3
Miha <stele1000@gmail.com>
2024-08-19 17:18:51 +0200

    WIP: Chapter 16

--- src/chapter16_fearless_concurrency.rs
+++ src/chapter16_fearless_concurrency.rs
@@ -30,4 +30,24 @@ pub fn using_threads_to_run_code_simultaneously() {
 
     // wait for the spawned thread to finish (blocks the main thread)
     handle.join().unwrap();
+
+
+    // let v = vec![1, 2, 3];
+    //
+    // let handle = thread::spawn(|| {
+    //     println!("Here's a vector: {v:?}");
+    // });
+    //
+    // drop(v); // oh no!
+    //
+    // handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    // Move transfers ownership so that the thread does not outlive the value like in the commented example where we drop the vector before it's finished
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {v:?}");
+    });
+
+    handle.join().unwrap();
 }
